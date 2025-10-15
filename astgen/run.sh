4b00b06c53691fa512303e462d9f2408c989353d
2IMT <daniil_t04@mail.ru>
2025-10-15 20:34:10 +0300

    Don't do backups when generating AST

--- astgen/run.sh
+++ astgen/run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-cp ../src/ast.h ../src/ast.h.bak
-cp ../src/ast.cpp ../src/ast.cpp.bak
 python3 astgen.py \
     --schema_file ../src/ast.json \
     --clang-format-config ../.clang-format \
