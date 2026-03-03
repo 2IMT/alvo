06d62bb0f25d495b9fd24f9d6c6cbb9d7c5db92f
2IMT <daniil_t04@mail.ru>
2026-03-03 17:29:11 +0300

    Add ALVO_UNREACHABLE() and ALVO_NOT_IMPLEMENTED()

--- src/util.cpp
+++ src/util.cpp
@@ -1,9 +1,28 @@
 #include "util.h"
 
+#include <iostream>
+#include <stdlib.h>
+
+#include <fmt/ostream.h>
+
 namespace alvo::util {
 
     void hash_combine(std::size_t& seed, std::size_t value) {
         seed ^= value + 0x9e3779b97f4a7c15ull + (seed << 6) + (seed >> 2);
     }
+
+    void unreachable(const char* file, int line, const char* func) {
+        fmt::print(std::cerr, "{}:{} encountered unreachable code in `{}`",
+            file, line, func);
+        alvo::util::abort();
+    }
+
+    void not_implemented(const char* file, int line, const char* func) {
+        fmt::print(std::cerr, "{}:{} encountered not implemented in `{}`", file,
+            line, func);
+        alvo::util::abort();
+    }
+
+    void abort() { std::abort(); }
 
 }
--- src/util.h
+++ src/util.h
@@ -11,4 +11,26 @@ namespace alvo::util {
     overload(Ts...) -> overload<Ts...>;
 
     void hash_combine(std::size_t& seed, std::size_t value);
+
+    void unreachable(const char* file, int line, const char* func);
+
+    void not_implemented(const char* file, int line, const char* func);
+
+    void abort();
 }
+
+#if ALVO_DEBUG == 1
+
+#  define ALVO_UNREACHABLE() \
+      ::alvo::util::unreachable(__FILE__, __LINE__, __func__)
+
+#  define ALVO_NOT_IMPLEMENTED() \
+      ::alvo::util::not_implemented(__FILE__, __LINE__, __func__)
+
+#else
+
+#  define ALVO_UNREACHABLE()
+
+#  define ALVO_NOT_IMPLEMENTED() ::alvo::util::abort()
+
+#endif
