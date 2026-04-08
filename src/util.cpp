61eb0ecfb729ed44b500f0f6d09d7f4e9bdc7b48
2IMT <daniil_t04@mail.ru>
2026-04-08 14:47:20 +0300

    Add ALVO_ASSERT

--- src/util.cpp
+++ src/util.cpp
@@ -1,7 +1,7 @@
 #include "util.h"
 
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
 
 #include <fmt/ostream.h>
 
@@ -11,18 +11,29 @@ namespace alvo::util {
         seed ^= value + 0x9e3779b97f4a7c15ull + (seed << 6) + (seed >> 2);
     }
 
-    void unreachable(const char* file, int line, const char* func) {
-        fmt::print(std::cerr, "{}:{} encountered unreachable code in `{}`",
+    void abort() { std::abort(); }
+
+#if ALVO_DEBUG == 1
+
+    void unreachable_(const char* file, int line, const char* func) {
+        fmt::println(std::cerr, "{}:{} encountered unreachable code in `{}`",
             file, line, func);
         alvo::util::abort();
     }
 
-    void not_implemented(const char* file, int line, const char* func) {
-        fmt::print(std::cerr, "{}:{} encountered not implemented in `{}`", file,
-            line, func);
+    void not_implemented_(const char* file, int line, const char* func) {
+        fmt::println(std::cerr, "{}:{} encountered not implemented in `{}`",
+            file, line, func);
         alvo::util::abort();
     }
 
-    void abort() { std::abort(); }
+    void assert_fail_(
+        const char* cond, const char* file, int line, const char* func) {
+        fmt::println(std::cerr, "{}:{} assertion `{}` failed in `{}`", file,
+            line, cond, func);
+        alvo::util::abort();
+    }
+
+#endif
 
 }
--- src/util.h
+++ src/util.h
@@ -12,25 +12,36 @@ namespace alvo::util {
 
     void hash_combine(std::size_t& seed, std::size_t value);
 
-    void unreachable(const char* file, int line, const char* func);
-
-    void not_implemented(const char* file, int line, const char* func);
-
     void abort();
-}
 
 #if ALVO_DEBUG == 1
 
 #  define ALVO_UNREACHABLE() \
-      ::alvo::util::unreachable(__FILE__, __LINE__, __func__)
+      ::alvo::util::unreachable_(__FILE__, __LINE__, __func__)
 
 #  define ALVO_NOT_IMPLEMENTED() \
-      ::alvo::util::not_implemented(__FILE__, __LINE__, __func__)
+      ::alvo::util::not_implemented_(__FILE__, __LINE__, __func__)
+
+#  define ALVO_ASSERT(cond_) \
+      (cond_) \
+          ? (void)0 \
+          : ::alvo::util::assert_fail_(#cond_, __FILE__, __LINE__, __func__)
+
+    void unreachable_(const char* file, int line, const char* func);
+
+    void not_implemented_(const char* file, int line, const char* func);
+
+    void assert_fail_(
+        const char* cond, const char* file, int line, const char* func);
 
 #else
 
 #  define ALVO_UNREACHABLE()
 
-#  define ALVO_NOT_IMPLEMENTED() ::alvo::util::abort()
+#  define ALVO_NOT_IMPLEMENTED()
+
+#  define ALVO_ASSERT(cond_)
 
 #endif
+
+}
